a671a4a0517cd8205437e20e589a684a632d235e
Smith Patel <96127171+iamsmith21@users.noreply.github.com>
2024-02-11 04:19:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import subprocess
-import downloadVideo
+
 
 def onSubmit():
     output_text.set("Fetching URL...")
@@ -12,6 +12,28 @@ def downloadAfterDelay():
     url = url_entry.get()
     downloadVideo(url)
     
+def downloadVideo(url):
+    command = f"yt-dlp -x -o \"%(title)s.%(ext)s\" --audio-format \"m4a\" --audio-quality 0 --embed-thumbnail --embed-metadata  --verbose '{url}'"
+    #print(command)
+    try:
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        while True:
+            line = process.stdout.readline()
+            if not line and process.poll() is not None:
+                break
+
+            # Display live output in a text widget
+            output_text.set(line.strip())
+            output_entry.update_idletasks()
+
+        # Check if the process completed successfully
+        if process.returncode == 0:
+            output_text.set("Audio Downloaded Successfully.")
+        else:
+            output_text.set(f"Error: {process.stderr.read().strip()}")
+        #todo : add setoutput var
+    except subprocess.CalledProcessError as e:
+        output_text.set(f"Error: {e.output}")
 root = tk.Tk()
 root.title("Youtube Downloader")
 root.minsize(400,400)
